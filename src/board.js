4fe802bb44a2ddd70eed930cfbba5390a01208e2
Ryan Hinkel <ryanhinkel@gmail.com>
2018-03-23 16:16:21 -0400

    Icey, commit one

---
+++ src/board.js
@@ -0,0 +1,56 @@
+export const boardSize = [50, 50]
+export const splitCoord = (coordString) => coordString.split(',').map(x => parseInt(x, 10))
+
+export const generateBoardCoordinates = () => {
+  const result = []
+  for (let y = 0; y < boardSize[1]; y++) {
+    for (let x = 0; x < boardSize[0]; x++) {
+      result.push(x + ',' + y)
+    }
+  }
+  return result
+}
+
+const renderHex = (state, coord) => {
+  const [x, y] = splitCoord(coord)
+
+  const moisture = state.moisture[coord]
+  const altitude = state.altitude[coord]
+  const temperature = state.temperature[coord]
+
+  const isGap = x == 0 && y % 2 == 0
+  const cls = isGap ? ' hex-gap' : ''
+
+  const color = temperature
+  // const bgColor = 'background-color: rgb(' + color + ', ' + color + ', ' + color + ');'
+  const bgColor = 'background-color: hsl(' + temperature + ', ' + altitude / 2.55 + '%, ' + moisture / 2.55 + '%);'
+  return (
+    '<div class="hex' + cls + '" style="' + bgColor + '">' +
+      '<div class="inner">' +
+        '<div style="color: rgb(0, 0,'+ moisture +')">'+
+          moisture +
+        '</div>' +
+        '<div style="color: rgb(0, '+ altitude +', 0)">'+
+          altitude +
+        '</div>' +
+        '<div style="color: rgb('+ temperature +', 0, 0)">'+
+          temperature +
+        '</div>' +
+      '</div>' +
+      '<div class="corner-1"></div>' +
+      '<div class="corner-2"></div>' +
+    '</div>'
+  )
+}
+
+export const renderBoard = (state) => {
+  const boardHtml = boardCoordinates.reduce((acc, coord) => {
+    acc += renderHex(state, coord)
+    return acc
+  }, '')
+
+  document.getElementById('board').innerHTML = boardHtml
+}
+
+
+export const boardCoordinates = generateBoardCoordinates()
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Simplex from 'perlin-simplex'
+
+import { renderBoard, splitCoord, boardCoordinates } from './board'
+
+import './hex.scss'
+import './board.scss'
+
+
+const createInitialState = (max = 1) => {
+  const s = new Simplex()
+  return boardCoordinates.reduce((acc, coord) => {
+    const [x, y] = splitCoord(coord)
+    acc[coord] = ((s.noise(x / 25, y / 25) + 1) / 2) * max
+    return acc
+  }, {})
+}
+
+const boardState = {
+  moisture: createInitialState(255),
+  altitude: createInitialState(255),
+  temperature: createInitialState(255),
+
+}
+
+const updateBoard = () => {
+  tile_moisture = boardState.moisture[coord]
+}
+
+
+renderBoard(boardState)
